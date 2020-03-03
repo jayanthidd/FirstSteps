0315c76e33de72267dc71b115c83fc603af11e0c
Miriam <mirithemidget@gmail.com>
2020-03-03 11:21:31 +0100

    Wir geben nun mehrere Zeilen aus

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class HelloWorld {
+    public static void main (String[] args) {
+        System.out.println("HelloWorld World");
+        int index = 0;
+        System.out.println("HelloWorld");
+        System.out.println("I am doing this myself");
+        return;
+        // Campus
+        // TODO: HelloWorld, I have to change this now
+    }
+}
